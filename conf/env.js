cde1a6d7d70246c827a10ed38fc1ad0d29247f02
Umayr Shahid <umayrr@hotmail.co.uk>
2015-05-27 03:26:55 +0500

    Added environments; settings for `dev`.

---
+++ conf/env.js
@@ -0,0 +1,19 @@
+/**
+ * Author: Umayr Shahid <umayrr@hotmail.com>,
+ * Created: 5:28 AM, 27/05/15.
+ */
+
+'use strict';
+
+var environments = {
+    dev: {
+        db: {
+            host: 'localhost',
+            name: 'hungrilla',
+            user: 'root',
+            password: ''
+        }
+    }
+};
+
+module.exports = environments;
