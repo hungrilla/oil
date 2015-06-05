31a723fcf1200802d0d45a9b60ea12d007f672a8
Umayr Shahid <umayrr@hotmail.co.uk>
2015-06-06 02:35:51 +0500

    Added model and repository for `menu`.

---
+++ repositories/menu.js
@@ -0,0 +1,49 @@
+'use strict';
+
+var model = require('../models/menu');
+
+module.exports = {
+  sync: sync,
+  drop: drop,
+  insert: insert
+};
+
+/**
+ * Creates table if its not existed.
+ *
+ * @param [isForced=false] if `true`, it drops the table and create a new one.
+ * @returns {*|Promise}
+ */
+function sync(isForced) {
+  isForced = isForced || false;
+
+  return model.sync({force: isForced});
+}
+
+/**
+ * Simply drops the table in context.
+ *
+ * @returns {*|Promise}
+ */
+function drop() {
+  return model.drop();
+}
+
+/**
+ * Loosely syncs the table with database then inserts a row in the table.
+ *
+ * @param obj contains values to be inserted in an object literal.
+ * @returns {*|Promise}
+ */
+function insert(obj) {
+  return this.sync().then(function () {
+    return model.create({
+      restaurantId: obj.restaurantId,
+      type: obj.type,
+      name: obj.name,
+      description: obj.description,
+      serves: obj.serves,
+      price: obj.price
+    });
+  });
+}
