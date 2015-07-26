fe266c9506e9b962fa0a51f288b7b715d3ebd24a
Mohammad Umair Khan <mohammad.umair.k@gmail.com>
2015-07-27 00:16:21 +0500

    associating serving and menu

--- models/serving.js
+++ models/serving.js
@@ -2,17 +2,13 @@
 
 var Sequelize = require('sequelize');
 var sequelize = require('../conf/sequelize')();
-var Menu = require('../models/menu');
+var Menu = require('./menu');
 
 var Serving = sequelize.define('serving', {
   id: {
     type: Sequelize.UUID,
     primaryKey: true,
     defaultValue: Sequelize.UUIDV4
-  },
-  menuId:{
-    type: Sequelize.UUID,
-    unique: true
   },
   pristinePrice:{
     type: Sequelize.STRING(255)
@@ -28,6 +24,8 @@ var Serving = sequelize.define('serving', {
   }
 });
 
-Serving.hasMany(Menu);
+//associating Menu and Serving with a 1:M relationship
+Menu.hasMany(Serving, {foreignKey: 'menuId'});
+Serving.belongsTo(Menu, {foreignKey: 'menuId'});
 
 module.exports = Serving;
